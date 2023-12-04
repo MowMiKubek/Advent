edd57ffe85634776184c4456f9cbed21d9caa95b
Jakub Maciej Tkaczyk <83206616+MowMiKubek@users.noreply.github.com>
2023-12-04 15:08:09 +0100

    Advent03 not completed

--- advent03/main.cpp
+++ advent03/main.cpp
@@ -2,12 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <cctype>
+#include <map>
+#include <set>
 
 bool isSymbol(char c) {
-    return !isdigit(c) && c != '.';
+    return c == '*';
 }
 
-bool checkIfIsPartNumber(std::vector<std::string> &grid, int verticalIdx, int horizontalIdx) {
+int checkIfIsPartNumber(std::vector<std::string> &grid, int verticalIdx, int horizontalIdx) {
     const unsigned int horizontalLength = grid[0].size();
     const unsigned int verticalLength = grid.size();
 
@@ -22,18 +24,21 @@ bool checkIfIsPartNumber(std::vector<std::string> &grid, int verticalIdx, int ho
             int newHIdx = horizontalIdx + h;
 
             if (withinBounds(newVIdx, newHIdx) && isSymbol(grid[newVIdx][newHIdx])) {
-                return true;
+                return newVIdx * grid[0].size() + newHIdx;
             }
         }
     }
-    return false;
+    return -1;
 }
 
+typedef unsigned long int ulong;
+
 int main() {
+
     std::ifstream file;
     std::vector<std::string> grid;
     std::string line;
-    int sum = 0;
+    std::map<int, std::pair<ulong, int>> lookupStars;
     file.open("input.txt");
     if (!file.good()) {
         std::cerr << "File not found\n";
@@ -44,28 +49,49 @@ int main() {
     }
     file.close();
     for (int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[i].size(); j++) {
+            if(grid[i][j] == '*')
+                lookupStars[i*grid[0].size() + j] = std::make_pair(1, 0);
+        }
+    }
+    for (int i = 0; i < grid.size(); i++) {
         bool isPartNumber = false;
         std::string textNumber;
+        std::set<int> starIndices;
         for (int j = 0; j < grid[i].size(); j++) {
             if (isdigit(grid[i][j])) {
                 textNumber += grid[i][j];
-                if (checkIfIsPartNumber(grid, i, j))
+                int starIndex = checkIfIsPartNumber(grid, i, j);
+                if (starIndex != -1) {
                     isPartNumber = true;
+                    starIndices.insert(starIndex);
+                    std::cout << "Found value " << textNumber << " pushing index: " << starIndex << '\n';
+                }
             } else if (!textNumber.empty()) {
                 if (isPartNumber) {
-//                    std::cout << textNumber << ' ';
-                    sum += std::stoi(textNumber);
+                    std::cout << "saving(" << starIndices.size() << "):";
+                    for(auto c: starIndices) {
+                        std:: cout << c << ',';
+                        auto oldPair = lookupStars[c];
+                        ulong oldValue = oldPair.first;
+                        int oldCount = oldPair.second;
+                        lookupStars[c] = std::make_pair(oldValue * std::stol(textNumber), oldCount + 1);
+                    }
+                    std::cout << '\n';
                     isPartNumber = false;
                 }
                 textNumber = "";
             }
         }
-        if (!textNumber.empty() && isPartNumber) {
-            std::cout << textNumber << ' ';
-            sum += std::stoi(textNumber);
-        }
-//        std::cout << '\n';
     }
-    std::cout << "Suma: " << sum;
+    ulong sum = 0;
+    for(auto [key, value]: lookupStars) {
+        if(value.second == 2){
+            std::cout << "Index " << key << "\n";
+            std::cout << "value: " << value.first << " count: " << value.second << '\n';
+            sum += value.first;
+        }
+    }
+    std::cout << "sum: " << sum << '\n';
     return 0;
 }
